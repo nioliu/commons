2a3b3e8f1d4e4e280df3104c4f49ff91101b5c33
nioliu <nioliu@tencent.com>
2022-12-26 16:42:55 +0800

    implement error

--- errs/errors.go
+++ errs/errors.go
@@ -1,6 +1,7 @@
 package errs
 
 import (
+	"encoding/json"
 	"github.com/go-redis/redis/v8"
 )
 
@@ -14,16 +15,26 @@ type ErrRsp struct {
 	Description string `json:"description"`
 }
 
+func (e *ErrRsp) Error() string {
+	var e2 *ErrRsp
+	*e2 = *e
+	if e2 == nil {
+		e2 = GetDefaultErrRsp()
+	}
+	marshal, _ := json.Marshal(e2)
+	return string(marshal)
+}
+
 // GetDefaultErrRsp default
-func GetDefaultErrRsp() ErrRsp {
-	return ErrRsp{Code: 0, Description: "Unknown"}
+func GetDefaultErrRsp() *ErrRsp {
+	return &ErrRsp{Code: 0, Description: "Unknown"}
 }
 
 // GetInnerSystemStandardError get common error response.
-func GetInnerSystemStandardError(err error) (errRsp ErrRsp) {
+func GetInnerSystemStandardError(err error) (errRsp *ErrRsp) {
 	switch err {
 	case redis.Nil:
-		errRsp = ErrRsp{
+		errRsp = &ErrRsp{
 			Code:        1000,
 			Description: "not found in storage",
 		}
