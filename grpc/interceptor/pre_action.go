0e051ff4603bfd1a4731172c8d580d60a8c6ee66
nioliu <nioliunio@gmail.com>
2024-09-27 15:33:07 -0400

    add common pre action for golang

---
+++ grpc/interceptor/pre_action.go
@@ -0,0 +1,57 @@
+package interceptor
+
+import (
+	"context"
+	"github.com/nioliu/commons/component"
+	"github.com/nioliu/commons/errs"
+	"github.com/nioliu/commons/grpc/object"
+	"github.com/nioliu/commons/log"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/metadata"
+	"net/http"
+	"net/http/httputil"
+	"time"
+)
+
+func PreActionForGolang(ctx context.Context, req *http.Request, now time.Time, errRsp *errs.ErrRsp) (context.Context, *errs.ErrRsp, int) {
+	dumpRequest, err := httputil.DumpRequest(req, true)
+	if err == nil {
+		log.InfoWithCtxFields(ctx, "receive request", zap.String("request content", string(dumpRequest)))
+	}
+
+	// fill metadata
+	m := &metadata.MD{}
+	if err = object.SetRecMsgSecondTimeToMd(m, now.Unix()); err != nil {
+		log.ErrorWithCtxFields(ctx, "set time to md failed", zap.Error(err))
+
+		errRsp = &errs.ErrRsp{Code: 1, Description: "internal service error"}
+		return ctx, errRsp, http.StatusInternalServerError
+	}
+
+	if err = object.SetRecMsgMilliSecondTimeToMd(m, now.UnixMilli()); err != nil {
+		log.ErrorWithCtxFields(ctx, "set milli sec time failed", zap.Error(err))
+		errRsp = &errs.ErrRsp{Code: 1, Description: "internal service error"}
+		return ctx, errRsp, http.StatusInternalServerError
+	}
+
+	ctx = metadata.NewOutgoingContext(ctx, *m) // out stac
+	traceId := GetTraceId(req)
+	ctx = context.WithValue(ctx, "trace_id", traceId)
+	regionData := GetReginData(req)
+	ctx = context.WithValue(ctx, "region_data", regionData)
+}
+
+func GetTraceId(r *http.Request) string {
+	traceId := r.Header.Get("X-Trace-Id")
+	if traceId == "" {
+		traceId = component.CreateSnowflakeId("0")
+	}
+
+	return traceId
+}
+
+func GetReginData(r *http.Request) string {
+	regionData := r.Header.Get("X-Region-Data")
+
+	return regionData
+}
